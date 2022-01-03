04d727dbf8d9ea631d7d47f6435d2fff1550db33
starwalker00 <aaronwalkerup@gmail.com>
2022-01-03 11:41:18 +0100

    add TicketOffice contract informations

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -1,9 +1,10 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
-
+import ticketOfficeAbi from "./abis/ticketOffice.json";
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
+  ticketOffice: ticketOfficeAbi
 };
 
 export default abis;
--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,6 +2,7 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
+  ticketOffice: "0x5fbdb2315678afecb367f032d93f642f64180aa3"
 };
 
 export default addresses;
